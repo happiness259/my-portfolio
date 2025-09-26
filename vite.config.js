138fed7ab6a631f18f150e0da4ea07be9d4270d4
happiness259 <wanjiruhappiness@gmail.com>
2025-09-26 13:46:55 +0300

    changed my base route

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/my-portfolio/",
-  plugins: 
-  [
-    tailwindcss(),
-    react()],
+  plugins: [react()],
+  base: '/', // 👈 replace with your repo name
 })
